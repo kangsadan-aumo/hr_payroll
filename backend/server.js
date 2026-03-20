7177900e7ed7a2633bb2d084aef4321003c0b50e
kangsadan.aumo <kangsadan.aumo@gmail.com>
2026-03-20 11:37:52 +0700

    แก้ไขบัคส่งเมล1

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ dotenv.config();
 
 // EMAIL TRANSPORTER
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || 'smtp.gmail.com',
-    port: parseInt(process.env.SMTP_PORT) || 587,
-    secure: process.env.SMTP_SECURE === 'true',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.SMTP_USER || '',
         pass: process.env.SMTP_PASS || '',
